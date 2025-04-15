8990fd770f64dc9a9421d46f58ec3b799cba7520
joe ward <139893416+joewrdd@users.noreply.github.com>
2025-04-16 00:44:38 +0300

    Delete functions directory

--- functions/hello.js
+++
@@ -1,7 +0,0 @@
-// Simple serverless function for Netlify
-exports.handler = async function (event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello from AIWrdd serverless function!" }),
-  };
-};
